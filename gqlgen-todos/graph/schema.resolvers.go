34c0a672316386bde19d7b556e3aff1fe1a65cf1
masaya0521 <kaji0521km@gmail.com>
2022-02-23 01:57:36 +0900

    implement resolver

--- gqlgen-todos/graph/schema.resolvers.go
+++ gqlgen-todos/graph/schema.resolvers.go
@@ -6,18 +6,28 @@ package graph
 import (
 	"context"
 	"fmt"
+	"math/rand"
 
 	"github.com/masaya0521/gqlgen-todos/graph/generated"
 	"github.com/masaya0521/gqlgen-todos/graph/model"
 )
 
+//ここでデータ処理の実装
 func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
+	todo := &model.Todo{
+		Text: input.Text,
+		ID: fmt.Sprintf("T%d", rand.Int()),
+		User: &model.User{ID: input.UserID, Name: "user" + input.UserID},
+	}
+	r.todos = append(r.todos, todo)
+	return todo, nil
 }
 
 func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
+	return r.todos, nil
 }
+
+
 
 // Mutation returns generated.MutationResolver implementation.
 func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
